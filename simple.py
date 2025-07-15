1e9bb09a3834b06db9ace6dd8dbed61260bcbe38
Jake Bowhay <jb9.bowhay@gmail.com>
2025-07-15 21:14:44 +0100

    plot u as well

--- simple.py
+++ simple.py
@@ -38,15 +38,23 @@ for i in tqdm(range(1, nt)):
     phi[i, 1:, 1:] += (s*dt/dy) * (phi[i - 1, :-1, 1:] - phi[i - 1, 1:, 1:])
     h[i, :] = h[i - 1, :] + s*dt * phi[i-1, -1, :]/(phi_c - phi[i-1, -1, :])
 
-fig, ax = plt.subplots(figsize=(10, 8))
+fig, (ax, ax1) = plt.subplots(2, 1, figsize=(10, 8))
 
 im = ax.imshow(phi[0, :, :], extent=[0, L, 0, H])
+im2 = ax1.imshow(u*np.ones_like(phi[0, :, :]), extent=[0, L, 0, H])
 line = ax.plot(np.linspace(0, L, nx), h[0, :], color='red')
+line2 = ax1.plot(np.linspace(0, L, nx), h[0, :], color='red')
+ax.set_xlabel("x")
+ax.set_ylabel("y")
+ax1.set_xlabel("x")
+ax1.set_ylabel("y")
+ax1.set_title("$u$")
 
 def update(frame):
     im.set_array(phi[frame, :, :])
-    ax.set_title(f"Time: {frame * dt:.2f} seconds")
+    ax.set_title(f"Time: {frame * dt:.2f} seconds\n$\\phi$")
     line[0].set_ydata(h[frame, :])
+    line2[0].set_ydata(h[frame, :])
 
 ani = animation.FuncAnimation(plt.gcf(), update, frames=nt, interval=50)
 plt.show()
